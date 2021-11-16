c82de68fffa356f0fcfafa961007e7dea70ae084
user <y.dubovitsky@gmail.com>
2021-11-16 14:14:32 +0300

    added hero component

--- src/components/public/hero/hero.jsx
+++ src/components/public/hero/hero.jsx
@@ -1,9 +1,26 @@
 import style from './hero.module.css';
+import Roll from 'react-reveal/Roll';
 
 export default function Hero() {
   return (
     <div className={style.container}>
-      Hero
+      <Roll left>
+        <div className={style.info}>
+          <h1>It's Amazing</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          <div className={style.buttonContainer}>
+            <div className={style.button}>
+              <p><i className="fab fa-android"></i>Play Store</p>
+            </div>
+            <div className={style.button}>
+              <p><i class="fab fa-apple"></i>App Store</p>
+            </div>
+          </div>
+        </div>
+      </Roll>
+      <div className={style.hand}>
+        <img src="https://technext.github.io/polo/HTML/assets/img/iphone_hand.png" alt="" srcset="" />
+      </div>
     </div>
   )
 }
