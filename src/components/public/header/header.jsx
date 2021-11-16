2dd93beee3d0f7fa3bb30f58199acffd0ec20772
user <y.dubovitsky@gmail.com>
2021-11-16 16:26:10 +0300

    added scroll event in header component

--- src/components/public/header/header.jsx
+++ src/components/public/header/header.jsx
@@ -1,8 +1,32 @@
+import cn from 'classname';
+import { useRef, useEffect, useState } from 'react';
+
 import style from './header.module.css';
 
 export default function Header() {
+
+  const elRef = useRef(null);
+  const [isScrolled, setIsScrolled] = useState(0);
+
+  let scrollTop = null;
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
+  // Execute when component destroyed
+  useEffect(() => () => window.removeEventListener('scroll', handleScroll), []);
+
+  const handleScroll = (event) => {
+    scrollTop = event.srcElement.body.scrollTop;
+    setIsScrolled(window.pageYOffset - scrollTop);
+  }
+
   return (
-    <div className={style.container}>
+    <div ref={elRef} className={cn(
+      style.container,
+      isScrolled > scrollTop ? style.scrolled : ''
+    )}>
       <div className={style.logo}>Flashcards</div>
       <div className={style.nav}><i className="fas fa-bars"></i></div>
     </div>
