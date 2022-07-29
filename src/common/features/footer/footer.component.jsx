5c6541a8d8baed2262228fb56612e73779f42d51
user <y.dubovitsky@gmail.com>
2022-07-29 16:03:20 +0300

    Styles of the footer improved

--- src/common/features/footer/footer.component.jsx
+++ src/common/features/footer/footer.component.jsx
@@ -8,31 +8,30 @@ const Footer = () => {
       <div className={cn(style.upper)}>
         <div className={cn(style.tutotarial)}>
           <ul>
-            <li><h3>Best Tutorials</h3></li>
-            <li>How to Start a Blog</li>
-            <li>How to Start a Podcast</li>
-            <li>How to Choose a Domain Name</li>
+            <li><h3>Discover</h3></li>
+            <li>Create Flashcards</li>
+            <li>Mobile App</li>
           </ul>
         </div>
         <div className={cn(style.hosting)}>
           <ul>
-            <li><h3>Hosting For Bloggers</h3></li>
-            <li>Best Blog Hosting Sites</li>
-            <li>Bluehost Review</li>
-            <li>Kinsta Review</li>
+            <li><h3>Company</h3></li>
+            <li>About Us</li>
+            <li>FAQ</li>
+            <li>Privacy Policy</li>
           </ul>
         </div>
       </div>
       <div className={cn(style.bottom)}>
         <ul>
-          <li>Blog</li>
-          <li>About us</li>
-          <li>What We Recommend</li>
+          <li>Home</li>
+          <li>Feature</li>
+          <li>Gallery</li>
         </ul>
         <ul>
-          <li>Free Themes</li>
-          <li>Documentation</li>
-          <li>Support</li>
+          <li>Contacts</li>
+          <li>Developer</li>
+          <li>Testimonial</li>
         </ul>
         <ul>
           <li>Terms and Conditions</li>
@@ -40,14 +39,14 @@ const Footer = () => {
           <li>Support Policy</li>
         </ul>
         <div className={style.social}>
-          {/* FIXME */}
-            {/* <div><i><FontAwesomeIcon icon={faTwitterSquare} /></i></div>
-            <div><i><FontAwesomeIcon icon={faFacebookSquare} /></i></div>
-            <div><i><FontAwesomeIcon icon={faInstagramSquare} /></i></div>
-            <div><i><FontAwesomeIcon icon={faVk} /></i></div> */}
+          <div><i class="fab fa-telegram"></i></div>
+          <div><i class="fab fa-vk"></i></div>
+          <div><i class="fab fa-instagram"></i></div>
+          <div><i class="fab fa-facebook"></i></div>
+          <div><i class="fab fa-blogger"></i></div>
         </div>
         <div className={cn(style.time)}>
-          <p>© 2017–{new Date().getFullYear()}, Engineer blog, official site</p>
+          <p>© 2021–{new Date().getFullYear()}, MemoRush, official site</p>
         </div>
       </div>
     </div>
