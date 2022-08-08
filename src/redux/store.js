ecd78ec12a13a1cfa876b1d752f08f282655eb22
user <y.dubovitsky@gmail.com>
2022-08-08 14:19:48 +0300

    Now auth state keeps into localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import PopupMiddleware from './middleware/popup.middleware';
+import { loadState, saveState } from '../service/localStorageService';
 import authReducer from './features/auth/auth-slice';
 import cardReducer from './features/card/cardSlice';
 import popupReducer from './features/popup/popup-slice';
+
+const persistedState = loadState('auth');
 
 const store = configureStore({
   reducer: {
@@ -10,7 +13,17 @@ const store = configureStore({
     card: cardReducer,
     popup: popupReducer
   },
+  preloadedState: {
+    auth: persistedState
+  },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(PopupMiddleware)
 })
+
+
+//TODO Доработать, чтобы можно было передавать объект с множеством полей!
+//! Save auth state
+store.subscribe(() => {
+  saveState('auth', store.getState().auth);
+});
 
 export default store;
