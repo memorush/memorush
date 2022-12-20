aa1dfe2728548335af9f3bdeec3f03e64245933a
user <y.dubovitsky@gmail.com>
2022-12-20 16:13:46 +0300

    Styles for card-set improved

--- src/pages/dashboard/components/card-set/card-set.component.jsx
+++ src/pages/dashboard/components/card-set/card-set.component.jsx
@@ -9,9 +9,17 @@ const CardSet = () => {
 
   return (
     <div className={styles.container}>
+      <div className={styles.containerTitle}>
+        <h1>Name of cardset</h1>
+        <p>isFavorite <i className="fas fa-heart"></i></p>
+        <p>author</p>
+        <p>createDate</p>
+        <p>updateDate</p>
+      </div>
       <div className={styles.actionButtons}>
         <Button name="Карточки" handler={() => navigate(`list`)} />
         <Button name="Учить!" handler={() => navigate(`memorize`)} />
+        <Button name="Главное меню" handler={() => navigate(`memorize`)} />
       </div>
       <div className={styles.content}>
         <Outlet />
--- src/pages/dashboard/components/card-set/flash-card-list/flash-card-list.component.jsx
+++ src/pages/dashboard/components/card-set/flash-card-list/flash-card-list.component.jsx
@@ -21,7 +21,21 @@ const FlashCardList = () => {
 
   return (
     <div className={styles.container}>
-      {showFlashCardItems()}
+      <div className={styles.containerTitle}>
+        <h1>{cardSetId}</h1>
+        <h3>12 Cards in this Set</h3>
+      </div>
+      <div className={styles.actionsButtons}>
+        <p>Press on a card for turning on</p>
+        <div className={styles.buttons}>
+          <i className="fas fa-edit"> Edit</i>
+          <i className="fas fa-print"> Print</i>
+          <i className="fas fa-file-export"> Export</i>
+        </div>
+      </div>
+      <div className={styles.cardsContainer}>
+        {showFlashCardItems()}
+      </div>
     </div>
   )
 }
