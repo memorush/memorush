77792306facfd7f61bc706e2d4f1f8c61ba86e92
user <y.dubovitsky@gmail.com>
2022-12-20 16:23:55 +0300

    Styles for settings page improved

--- src/pages/dashboard/components/settings/settings.component.jsx
+++ src/pages/dashboard/components/settings/settings.component.jsx
@@ -22,26 +22,34 @@ const Settings = () => {
 
   return (
     <div className={styles.container}>
-      <label htmlFor='title'>Ваш пароль</label>
-      <input
-        type="password"
-        name="password"
-        onChange={onInputDataChangedHandler}
-      />
-      <label htmlFor='title'>Введите пароль еще раз</label>
-      <input
-        type="password"
-        name="password2"
-        onChange={onInputDataChangedHandler}
-      />
-      <label htmlFor='title'>Ваш email</label>
-      <input
-        type="email"
-        name="email"
-        onChange={onInputDataChangedHandler}
-      />
-      <Button name="Обновить данные пользователя" handler={onUpdateDataHandler} />
-    </div>
+      <div className={styles.containerTitle}>
+        <h1>Настройки пользователя</h1>
+      </div>
+      <div className={styles.settingsContainer}>
+        <label htmlFor='title'>Ваш пароль</label>
+        <input
+          type="password"
+          name="password"
+          onChange={onInputDataChangedHandler}
+        />
+        <label htmlFor='title'>Введите пароль еще раз</label>
+        <input
+          type="password"
+          name="password2"
+          onChange={onInputDataChangedHandler}
+        />
+        <label htmlFor='title'>Ваш email</label>
+        <input
+          type="email"
+          name="email"
+          onChange={onInputDataChangedHandler}
+        />
+        <div className={styles.actionButtons}>
+          <Button name="Обновить данные пользователя" handler={onUpdateDataHandler} />
+          <Button name="Главное меню" handler={onUpdateDataHandler} />
+        </div>
+      </div>
+    </div >
   )
 }
 
