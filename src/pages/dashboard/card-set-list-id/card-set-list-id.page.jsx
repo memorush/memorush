c4d01ad47e9c99f91908fbe659155b00e14e01b5
user <y.dubovitsky@gmail.com>
2022-12-22 12:35:25 +0300

    Data for card-set-list-id.page takes from redux, so they are dynamic now

--- src/pages/dashboard/card-set-list-id/card-set-list-id.page.jsx
+++ src/pages/dashboard/card-set-list-id/card-set-list-id.page.jsx
@@ -1,4 +1,6 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { cardSetByIdSelector } from '../../../redux/features/card-set/card-set.slice';
 import ButtonAtomicComponent from '../../../common/atomic-components/button/button.component';
 import styles from './card-set-list-id.module.css';
 import { ReactComponent as LearningSvg } from './svg/learning-student-svgrepo-com.svg';
@@ -6,6 +8,15 @@ import { ReactComponent as LearningSvg } from './svg/learning-student-svgrepo-co
 const CardSetListIdPage = () => {
 
   const navigate = useNavigate();
+  const { cardSetId } = useParams();
+
+  // Get data for card-set set with id = ...
+  const {
+    name,
+    createdAt,
+    updatedAt,
+    isFavorite
+  } = useSelector(state => cardSetByIdSelector(state, cardSetId));
 
   return (
     <div className={styles.container}>
@@ -14,13 +25,12 @@ const CardSetListIdPage = () => {
           <div className={styles.svgContainer}>
             <LearningSvg />
           </div>
-          <h1>Name of cardset</h1>
+          <h1>{name}</h1>
         </div>
         <div className={styles.mainInfo}>
-          <p>isFavorite <i className="fas fa-heart"></i></p>
-          <p>author</p>
-          <p>createDate</p>
-          <p>updateDate</p>
+          <p>Is favorite? <i className="fas fa-heart" style={{ color: isFavorite ? 'red' : '' }}></i></p>
+          <p>Create Date: <span>{createdAt}</span></p>
+          <p>Update Date: {updatedAt}</p>
         </div>
       </div>
       <div className={styles.actionButtons}>
