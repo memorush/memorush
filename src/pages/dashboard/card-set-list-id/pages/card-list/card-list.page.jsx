b8538e745ae1f90815fd8512e44fe4f55175c3c3
user <y.dubovitsky@gmail.com>
2022-12-22 11:53:04 +0300

    Features for export, edit and print card set added

--- src/pages/dashboard/card-set-list-id/pages/card-list/card-list.page.jsx
+++ src/pages/dashboard/card-set-list-id/pages/card-list/card-list.page.jsx
@@ -1,13 +1,17 @@
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useRef } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import {
   cardSetByIdSelector
 } from '../../../../../redux/features/card-set/card-set.slice';
+import { useReactToPrint } from 'react-to-print';
 import CardItem from './components/card-item/card-item.component';
 import styles from "./card-list.module.css";
 
 const CardListPage = () => {
 
+  const printRef = useRef();
+  const navigate = useNavigate();
   const { cardSetId } = useParams();
 
   // Get data for card-set set with id = ...
@@ -19,6 +23,26 @@ const CardListPage = () => {
     ))
   )
 
+  const saveCardSetToFile = (content, fileName, contentType) => {
+    var a = document.createElement("a");
+    var file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+  }
+
+  const saveCardSetHandler = () => {
+    saveCardSetToFile(JSON.stringify(cardSetById), `card-set-${cardSetId}.txt`, 'text/plain')
+  }
+
+  const printHandler = useReactToPrint({
+    content: () => printRef.current
+  })
+
+  const navigateToEditFormHandler = () => {
+    navigate(`/dashboard/card-set-edit?id=${cardSetId}`)
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.containerTitle}>
@@ -28,12 +52,12 @@ const CardListPage = () => {
       <div className={styles.actionsButtons}>
         <p>Press on a card for turning on</p>
         <div className={styles.buttons}>
-          <i className="fas fa-edit"> Edit</i>
-          <i className="fas fa-print"> Print</i>
-          <i className="fas fa-file-export"> Export</i>
+          <i className="fas fa-edit" onClick={navigateToEditFormHandler}> Edit</i>
+          <i className="fas fa-print" onClick={printHandler}> Print</i>
+          <i className="fas fa-file-export" onClick={saveCardSetHandler}> Export</i>
         </div>
       </div>
-      <div className={styles.cardsContainer}>
+      <div className={styles.cardsContainer} ref={printRef}>
         {showFlashCardItems()}
       </div>
     </div>
