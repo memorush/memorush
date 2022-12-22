21801d1ebe6e8dd44d5e311624eaf4df6c573bd5
user <y.dubovitsky@gmail.com>
2022-12-22 10:32:14 +0300

    Logout feature added

--- src/layouts/dashboard/components/nav-header/nav-header.component.jsx
+++ src/layouts/dashboard/components/nav-header/nav-header.component.jsx
@@ -1,7 +1,15 @@
 import styles from './nav-header.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { invalidateLoggedInUser } from '../../../../redux/features/auth/auth-slice';
 import { Link } from 'react-router-dom';
 
 const NavHeaderComponent = () => {
+
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(invalidateLoggedInUser());
+  }
 
   return (
     <div className={styles.container}>
@@ -9,7 +17,7 @@ const NavHeaderComponent = () => {
         <Link to={"/"}><i className="fas fa-brain"></i> Memorush</Link>
       </div>
       <div className={styles.userContainer}>
-        <i className="fas fa-sign-out-alt"></i>
+        <i className="fas fa-sign-out-alt" onClick={logout}></i>
       </div>
     </div>
   )
