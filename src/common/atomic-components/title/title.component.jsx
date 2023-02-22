0d98da2aa73677fa0a240abc2092502fac945a18
user <y.dubovitsky@gmail.com>
2023-02-22 16:50:40 +0300

    TitleAtomicComponent has been wrapped by div again

--- src/common/atomic-components/title/title.component.jsx
+++ src/common/atomic-components/title/title.component.jsx
@@ -2,12 +2,12 @@ import styles from './title.module.css';
 
 const TitleAtomicComponent = ({ name, style }) => {
   return (
-    // <div
-    //   style={{ ...style }}
-    //   className={styles.container}
-    // >
+    <div
+      style={{ ...style }}
+      className={styles.container}
+    >
       <h1 className={styles.textSize}>{name}</h1>
-    // </div>
+    </div>
   )
 }
 
