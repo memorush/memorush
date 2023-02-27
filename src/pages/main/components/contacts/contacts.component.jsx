cd27af63ec167e9ae0a4b51e15119b5235741e03
user <y.dubovitsky@gmail.com>
2023-02-27 10:21:45 +0300

    FormAction added for sending email in ContactsComponent on the main page

--- src/pages/main/components/contacts/contacts.component.jsx
+++ src/pages/main/components/contacts/contacts.component.jsx
@@ -1,23 +1,23 @@
-import style from './contacts.module.css';
+import styles from './contacts.module.css';
 import ButtonAtomicComponent from '../../../../common/atomic-components/button/button.component';
 import TitleAtomicComponent from '../../../../common/atomic-components/title/title.component';
 import { Fade, Slide } from "react-awesome-reveal";
 
 const ContactsComponent = () => {
   return (
-    <div id="contacts" className={style.container}>
+    <div id="contacts" className={styles.container}>
       <Fade>
         <TitleAtomicComponent name="Обратная связь" style={{ textAlign: 'center', color: "white" }} />
       </Fade>
-      <div className={style.info}>
+      <div className={styles.info}>
         <Slide direction='left'>
-          <div className={style.contactContainer}>
-            <div className={style.contactItem}>
+          <div className={styles.contactContainer}>
+            <div className={styles.contactItem}>
               <i className="fas fa-envelope-open"></i>
               <h4>Почта</h4>
               <p>y.dubovitsky@gmail.com</p>
             </div>
-            <div className={style.contactItem}>
+            <div className={styles.contactItem}>
               <i className="fab fa-internet-explorer"></i>
               <h4>Вебсайт</h4>
               <p>www.ydubovitsky.ru</p>
@@ -25,12 +25,17 @@ const ContactsComponent = () => {
           </div>
         </Slide>
         <Slide direction='right'>
-          <div className={style.formContainer}>
-            <input type="text" placeholder="Ваше имя"></input>
-            <input type="text" placeholder="Ваша почта"></input>
-            <input type="text" placeholder="Тема письма"></input>
-            <input type="textarea" placeholder="Ваше сообщение"></input>
-            <ButtonAtomicComponent name="Отправить" />
+          <div className={styles.formContainer}>
+            <form action="mailto:y.dubovitsky@gmail.com">
+              <input type="text" placeholder="Ваше имя"></input>
+              <input type="text" placeholder="Ваша почта"></input>
+              <input type="text" placeholder="Тема письма"></input>
+              <input type="textarea" placeholder="Ваше сообщение"></input>
+              <div className={styles.buttons}>
+                <input type="submit" value="Отправить"></input>
+                <input type="reset" value="Очистить"></input>
+              </div>
+            </form>
           </div>
         </Slide>
       </div>
