2817d0722d194c312466d634eefc71db48dc10a7
user <y.dubovitsky@gmail.com>
2023-03-16 18:13:35 +0300

    Swipe event for sidebar added

--- src/common/components/sidebar/sidebar.component.jsx
+++ src/common/components/sidebar/sidebar.component.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react';
 import cn from 'classnames';
-import styles from './sidebar.module.css';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { invalidateLoggedInUser, authEntitySelector } from '../../../redux/features/auth/auth-slice';
+import { Link, useNavigate } from 'react-router-dom';
+import { authEntitySelector, invalidateLoggedInUser } from '../../../redux/features/auth/auth-slice';
+import styles from './sidebar.module.css';
 
 
 const SidebarComponent = () => {
+
+  // Swipe variables
+  let xDown = null;
+  let yDown = null;
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -25,8 +28,37 @@ const SidebarComponent = () => {
     }
   }
 
+  const handleTouchStart = (evt) => {
+    const firstTouch = evt.touches[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+  };
+
+  const handleTouchMove = (evt) => {
+    if (!xDown || !yDown) {
+      return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      if (xDiff < 0) {
+        setIsOpen(false);
+      }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+  };
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container}
+      onTouchMove={handleTouchMove}
+      onTouchStart={handleTouchStart}>
       <div className={cn(styles.sidebar, isOpen ? styles.open : null)}>
         <div className={styles.toggle} onClick={toggleSidebarHandler}>
           <i className={cn("fas fa-hand-pointer", styles.pointer)}></i>
