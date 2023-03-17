fac99a346a88d5fd9f9fd75c8ec29cf0be05ab64
user <y.dubovitsky@gmail.com>
2023-03-17 12:32:32 +0300

    disable prop added to ButtonAtomicComponent

--- src/common/atomic-components/button/button.component.jsx
+++ src/common/atomic-components/button/button.component.jsx
@@ -5,12 +5,12 @@ import styles from './button.module.css';
  * handler - this arg applies more than one functions, for example: onPress, onChange and etc
  * @returns 
  */
-const ButtonAtomicComponent = ({ name, style, color, clickFunction }) => {
+const ButtonAtomicComponent = ({ name, style, color, clickFunction, disable = false }) => {
   return (
     <div data-testid="button-atomic-component"
       className={styles.container}
-      style={{ background: color, ...style }}
-      onClick={clickFunction}
+      style={disable ? { background: 'silver' } : { background: color, ...style }}
+      onClick={disable ? null : clickFunction}
     ><p>{name}</p>
     </div>
   )
