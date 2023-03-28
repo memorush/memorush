5c30611c69b6aea75bddd674b24399afe22987b4
user <y.dubovitsky@gmail.com>
2023-03-28 13:18:54 +0300

    Added qr code for the app and downloads styles improved

--- src/pages/main/components/download/download.component.jsx
+++ src/pages/main/components/download/download.component.jsx
@@ -1,7 +1,9 @@
 import styles from './download.module.css';
+import { Link } from 'react-router-dom';
 import ButtonAtomicComponent from '../../../../common/atomic-components/button/button.component';
 import { Fade } from "react-awesome-reveal";
 import TitleAtomicComponent from '../../../../common/atomic-components/title/title.component';
+import qrConde from './images/qr-code.png'
 
 const DownloadComponent = () => {
   return (
@@ -11,11 +13,14 @@ const DownloadComponent = () => {
           <TitleAtomicComponent name="Установите приложение" />
           <div className={styles.line} />
           <p>Установите наше приложение и начните учиться прямо сейчас!</p>
+          <p className={styles.avaliableText}>«Приложение временно доступно только на Android устройствах...»</p>
           <div className={styles.line} />
         </div>
         <div className={styles.buttonContainer}>
-          <ButtonAtomicComponent name={"Play Market"} />
-          <ButtonAtomicComponent name={"App Store"} />
+          <img className={styles.qrConde} src={qrConde} alt="" />
+          <Link to={process.env.REACT_APP_APPGALLERY_LINK} target="_blank">
+            <ButtonAtomicComponent name={"App Gallery"} />
+          </Link>
         </div>
       </Fade>
       <div className={styles.downloadIcon}>
