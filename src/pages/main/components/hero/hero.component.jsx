209760288780de9977a88016c263c4a2842e0d47
user <y.dubovitsky@gmail.com>
2023-06-13 14:44:13 +0300

    HeroComponent redesigned with Spline 3D

--- src/pages/main/components/hero/hero.component.jsx
+++ src/pages/main/components/hero/hero.component.jsx
@@ -1,46 +1,11 @@
-import { useEffect, useState } from 'react';
-import { Fade, Slide } from "react-awesome-reveal";
+import Spline from '@splinetool/react-spline';
 import styles from './hero.module.css';
-import repeatImage from "./images/hardik-sharma-CrPAvN29Nhs-unsplash.jpg";
-
-import cn from 'classnames';
 
 const HeroComponent = () => {
 
-  const [heroText, setHeroText] = useState(" ");
-  const text = "...cкачайте приложение или воспользуйтесь веб версией!";
-  const [idx, setIdx] = useState(0);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (idx === text.length) {
-        setIdx(0);
-        setHeroText(" ");
-      } else {
-        setIdx(prev => prev + 1);
-        setHeroText((prev) => prev + text.charAt(idx));
-      }
-    }, 100);
-    return () => clearTimeout(timeout);
-  }, [heroText]);
-
   return (
     <div id="hero" className={styles.container}>
-      <div className={cn(styles.column, styles.left)}>
-        <Fade>
-          <h1>Учитесь просто, быстро и эффективно вместе с </h1>
-        </Fade>
-        <Fade>
-          <h1 className={styles.logo}>Memo<span className={styles.r}>R</span>ush</h1>
-        </Fade>
-        <Slide direction='down'>
-          <h2>Интерактивный тренажер для запоминания слов!</h2>
-        </Slide>
-        <h4 className={styles.heroText}>{heroText}</h4>
-      </div>
-      <div className={cn(styles.column, styles.right)}
-        style={{ backgroundImage: `url(${repeatImage})` }}>
-      </div>
+      <Spline scene="https://prod.spline.design/Fyi3nNUHeBsQH62V/scene.splinecode" />
     </div>
   )
 }
